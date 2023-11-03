dbf5ce34fe93d04b9bcf79ea2fe252df6c4fba62
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2023-11-03 14:24:10 +0000

    Copilot second commit correction

--- member.js
+++ member.js
@@ -1,15 +1,25 @@
-function skillMember() {
-    var member = document.getElementById("member");
-    var memberValue = member.value;
-    var memberLength = memberValue.length;
-    var memberMessage = document.getElementById("memberMessage");
-    if (memberLength < 1) {
-        memberMessage.innerHTML = "请输入会员名";
-        memberMessage.style.color = "#FF0000";
-        return false;
-    } else {
-        memberMessage.innerHTML = "OK";
-        memberMessage.style.color = "#00FF00";
-        return true;
-    }
-}
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django'];
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: skills,
+        // skills: skillsMember,
+        // skills: function () {
+        //     return skills;
+        // },
+        // skills: () => {
+        //     return skills;
+        // },
+        // skills: () => skills,
+        // skills: function () {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => this.skills,
+        // skills: function () {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => this.skills,
+        // skills: () => this.skills,
